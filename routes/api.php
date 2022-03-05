404303916e958d72e3ecf60f2491122d5abd9444
Obolduy <ivanox01@yandex.ru>
2022-03-05 19:41:25 +0300

    Роутинг

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\{AddQuoteController, ShowQuotesController};
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/add', [AddQuoteController::class, 'add']);
-Route::get('/test', [ShowQuotesController::class, 'showQuotes']);
+Route::post('/add-quote', [AddQuoteController::class, 'add']);
+Route::post('/check-author', [AddQuoteController::class, 'checkAuthor']);
+Route::get('/show-quotes', [ShowQuotesController::class, 'showQuotes']);
+Route::get('/show-quote/{id}', [ShowQuotesController::class, 'showQuote']);
