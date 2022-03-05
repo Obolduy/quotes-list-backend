3af66e306e3bd0e6ce496d1f7e4ef1e638cf2e83
Obolduy <ivanox01@yandex.ru>
2022-03-05 15:44:33 +0300

    Метод показа 10 цитат в ShowQuotesController

---
+++ app/Http/Controllers/ShowQuotesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\{TagsNames, Quotes};
+use Illuminate\Http\Request;
+
+class ShowQuotesController extends Controller
+{
+    public function showQuotes() {
+        $quotes = Quotes::select('quotes.text', 'quotes.created_at', 'quotes.id as quote_id', 'authors.author')
+                        ->join('authors', 'quotes.author_id', 'authors.id')
+                        ->orderByDesc('quotes.created_at')
+                        ->paginate(10);
+
+        $tags = [];
+
+        foreach ($quotes as $quote) {
+            $tags[] = TagsNames::select('tags_names.tag')
+                                ->join('tags_list', 'tags_names.id', 'tags_list.tag_id')
+                                ->where('tags_list.quote_id', $quote->quote_id)
+                                ->get();
+        }
+
+        return response()->json(['quotes' => $quotes, 'tags' => $tags]);
+    }
+}
