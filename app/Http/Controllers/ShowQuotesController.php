3bd9202c0b127be0477291cccb187f5eef2e71cb
Obolduy <ivanox01@yandex.ru>
2022-03-05 19:33:44 +0300

    Показ одной цитаты в ShowQuotesController

--- app/Http/Controllers/ShowQuotesController.php
+++ app/Http/Controllers/ShowQuotesController.php
@@ -24,4 +24,18 @@ class ShowQuotesController extends Controller
 
         return response()->json(['quotes' => $quotes, 'tags' => $tags]);
     }
+
+    public function showQuote(int $id) {
+        $quote = Quotes::select('quotes.*', 'authors.author')
+                        ->join('authors', 'quotes.author_id', 'authors.id')
+                        ->where('quotes.id', $id)
+                        ->first();
+
+        $tags = TagsNames::select('tags_names.tag')
+                            ->join('tags_list', 'tags_names.id', 'tags_list.tag_id')
+                            ->where('tags_list.quote_id', $quote->id)
+                            ->get();
+
+        return response()->json(['quote' => $quote, 'tags' => $tags]);
+    }
 }
